6355aa4c5028b6a9b0c3c8eb6b3dfcdc767533de
Grain Stark <palagend@gmail.com>
2018-09-12 14:39:12 +0800

    初始化项目

---
+++ src/main/java/io/fzyun/paas/stackservice/StackServiceApplication.java
@@ -0,0 +1,12 @@
+package io.fzyun.paas.stackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackServiceApplication.class, args);
+    }
+}
