7d0d73b50d8859242d54c7444c4ec5651b128c80
Grain Stark <palagend@gmail.com>
2018-09-13 18:21:46 +0800

    调整mvnw

--- cmd/build.sh
+++ cmd/build.sh
@@ -2,5 +2,4 @@
 set -e
 test ${WORKSPACE:?WORKSPACE should been set!}
 # 构建stack-services
-curl -ulib:Founder123 -L# -o ${WORKSPACE}/.mvn/wrapper/maven-wrapper.jar ftp://ftp.fzyun.io/apache/maven-wrapper.jar
 ${WORKSPACE}/mvnw clean install -DskipTests -Pjenkins
--- cmd/deploy.sh
+++ cmd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 test ${WORKSPACE:?WORKSPACE MUST be set!}
-# 部署stack-services
+# 部署stack-service
 rancher-compose -p ycbs -f ${WORKSPACE}/compose/docker-compose.yml -r ${WORKSPACE}/compose/rancher-compose.yml up -d -p -c --force-recreate
